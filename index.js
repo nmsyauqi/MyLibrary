aeb4608c5c0a7abb0c30db01d6be759aa441d7b4
nmsyauqi <syauqimuhammad430@gmail.com>
2026-08-09 15:55:46 +0700

    -seca push:INDEX.JS

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ for (let i = 0; i < headerSmallMenuLinks.length; i++) {
 const headerLogoConatiner = document.querySelector('.header__logo-container')
 
 headerLogoConatiner.addEventListener('click', () => {
-  location.href = 'http://nmsyauqi.my.id'
+  location.href = '/'
 })
 
 async function loadPage(pageName, hash = '') {
